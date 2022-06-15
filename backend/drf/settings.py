e8fdde9612c9bbab3bc8edb6bd3e467ec3d60e60
Aimen Yaseen <aimenyaseen6@gmail.com>
2022-06-15 16:48:30 +0500

    modify django authentication schema from username based to email based

--- backend/drf/settings.py
+++ backend/drf/settings.py
@@ -128,6 +128,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+AUTH_USER_MODEL = 'users.User'
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
